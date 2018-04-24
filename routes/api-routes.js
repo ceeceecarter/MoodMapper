f4beec936879665e6559593a8fffc224bfb6fe76
Elizabeth <erischjanson@gmail.com>
2018-04-23 20:40:08 -0500

    update models and apis

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,18 +13,12 @@ module.exports = function(app){
     app.post("/api/survey", function(req, res){
 
         db.Mood.create({
-            moodType: req.body.type,
-            answer1: req.body.X,
-            answer2: req.body.X,
-            answer3: req.body.X,
-            answer4: req.body.X,
-            answer5: req.body.X,
-            answer6: req.body.X,
-            answer7: req.body.X,
-            answer8: req.body.X,
-            answer9: req.body.X,
-            answer10: req.body.X,
-            usersssssss: XXXX            
+            anxiety: req.body.X,
+            depression: req.body.X,
+            concentration: req.body.X,
+            energy: req.body.X,
+            happiness: req.body.X,
+            // usersssssss: XXXX            
         }).then(function(data){
             res.json(data);
         });

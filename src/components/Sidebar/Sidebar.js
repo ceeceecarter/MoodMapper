7fa30c38b545c2da0430e3dc1c6f6e3f044b6720
Elizabeth <erischjanson@gmail.com>
2018-04-26 17:40:01 -0500

    api call works and returns quotes/data in console

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,8 +6,8 @@ const Sidebar = (props) => (
  <div className="Sidebar">
    
 {/* <p id="side-bar">Sidebar goes here</p> */}
-<h5 style={{"font-style": "italic"}}>Quote: {props.quoteText}</h5>
-<p style={{"font-style": "italic"}}>Author: {props.quoteAuthor}</p>
+<h5 style={{fontStyle: "italic"}}>Quote: {props.quoteText}</h5>
+<p style={{fontStyle: "italic"}}>Author: {props.quoteAuthor}</p>
   
 </div>
 );

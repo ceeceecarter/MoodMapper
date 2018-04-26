7f7fb201d4668db92f85d617c0290cac2cc4e212
Ben Cross <bencross323@gmail.com>
2018-04-25 20:51:46 -0500

    survey render

--- src/components/Survey/Quiz.js
+++ src/components/Survey/Quiz.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import Question from '../components/Question';
-import QuestionCount from '../components/QuestionCount';
-import AnswerOption from '../components/AnswerOption';
+import Question from './Question';
+import QuestionCount from './QuestionCount';
+import AnswerOption from './AnswerOption';
 
 function Quiz(props) {
 

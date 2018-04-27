bbc61eb1a205e714772114b6c4d834e742dcca81
Elizabeth <erischjanson@gmail.com>
2018-04-27 15:57:16 -0500

    api quote displaying in sidebar

--- src/components/Sidebar/SidebarContainer.js
+++ src/components/Sidebar/SidebarContainer.js
@@ -5,26 +5,46 @@ import "./Sidebar.css";
 import axios from "axios";
 
 class SidebarContainer extends Component {
-    state = {
-        result: {
-            
-                "quoteText": "Difficulties are things that show a person what they are.  ",
-                "quoteAuthor": "Epictetus ",
+    state = {        
+        result:{}
+        //    "quoteAuthor":"",
+           
+                // "quoteText": "Difficulties are things that show a person what they are.  ",
+                // "quoteAuthor": "Epictetus ",
                        
-        }     
+             
     };
 
-    componentDidMount() {
-       console.log("HEYYY");
-       this.searchQuotes("https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?");
+    componentDidMount(){
+        console.log("HII");
+        axios.get("https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&json=?")
+        .then(response => this.setState({result:response.data}))
     }
-    
 
-   searchQuotes = query => {
-       axios.get(query)
-       .then(res => console.log(res));
+    // componentDidMount() {
+    //    console.log("HEYYY");
+    //    this.searchQuotes("https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?");
+    // }
+    
+    // searchQuotes = query => {
+    //     axios.get(query)
+    //     .then((response) => {
+    //         console.log(response.data);
+    //         this.setState({quoteText:response.data.quoteText, quoteAuthor:response.data.quoteAuthor});
+    //       })
+    //      .catch((error)=>{
+    //         console.log(error);
+    //      });
+    // }
+
+//    searchQuotes = query => {
+//        axios.get(query)
+//        .then(res => this.setState({ result: res.data }))
+//        .catch(err => console.log(err));
        
-   };  
+//    };  
+
+
 
     render(){
         return (

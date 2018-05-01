fd76b8e6e09779e925173cd8cef598fb34bcfdc8
Elizabeth <erischjanson@gmail.com>
2018-04-30 20:02:55 -0500

    graph rendering

--- client/src/components/Pages/Graph.js
+++ client/src/components/Pages/Graph.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React,{Component} from "react";
+import { Chart } from 'react-google-charts';
 
-const Graph = () => (
-  <div>
-    <h1>Display Graph</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
-  </div>
-);
-
-export default Graph;
+export default class Graph extends Component {
+  constructor (props) {
+    //makes sure component gets props from parent
+    //super--calls the constructor of the parent class
+    super(props)
+    this.state = {
+      chartdata : [['Day', 'Anxiety', 'Depression'], [4, 8,3], [3, 6, 4]] 
+    }
+  }
+  render () {
+    return (
+      <div className = {'graph-results'}>
+        <Chart
+        chartType = "ScatterChart"
+        data ={this.state.chartdata}
+        options = {{ }}
+        graph_id = "ScatterChart"
+        width = "100%"
+        height = "400px"
+        overflow = "auto"
+        //legend_toggle is prop that sets prop value to true
+        legend_toggle
+        />
+      </div>
+    );
+  }
+}

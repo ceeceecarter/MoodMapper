6d4a59077cdce6570783b632fc11e0de4cee7b08
alyciamriley <alyciarileym@gmail.com>
2018-05-02 17:03:59 -0500

    Working on getting only one list item to drop down at a time.

--- client/src/components/Pages/Faq.js
+++ client/src/components/Pages/Faq.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 import "./PagesStyles/Faq.css";
 
+class Faq extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
 
-const Faq = () => (
-  <div>
-    <h1>Frequently Asked Questions</h1>
-    <ul>
-      <li>Who is this for?</li>
-      <p>Anyone who wants to track their mood or mental health over time. Our hope is you will begin to recognize patterns and make more informed decisions based on the details of your day.</p>
-      <li>How does this work?</li>
-      <p>Every day you can log in and take a short survey that will measure your feelings. You can also make journal entries. The survey responses will be graphed over time. In this way, you can look at a specific day on the graph to see the corresponding journal entries.</p>
-      <li>Does this replace seeing a mental health professional?</li>
-      <p>No. This is a tool you may choose to use with your therapist, but it is not intended to supplement professional mental health care.</p>
-      <li>Why don't I see my specific diagnosis on here?</li>
-      <p>We chose to focus on anxiety and depression because they are the most commonly diagnosed mood disorders.</p>     
-    </ul>
-  </div>
-);
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i></Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            Anim pariatur cliche reprehenderit,
+             enim eiusmod high life accusamus terry richardson ad squid. Nihil
+             anim keffiyeh helvetica, craft beer labore wes anderson cred
+             nesciunt sapiente ea proident.
+            </CardBody>
+          </Card>
+        </Collapse>
+      </div>
+    );
+  }
+}
+
 export default Faq;

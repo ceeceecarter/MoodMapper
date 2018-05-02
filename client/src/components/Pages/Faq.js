f6dabd778e92fd7ead04938a5536eaf573512915
alyciamriley <alyciarileym@gmail.com>
2018-05-02 18:19:42 -0500

    basic formatting is there.

--- client/src/components/Pages/Faq.js
+++ client/src/components/Pages/Faq.js
@@ -16,17 +16,36 @@ class Faq extends Component {
   render() {
     return (
       <div>
-        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i></Button>
+        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Who is this for?</span> </Button>
         <Collapse isOpen={this.state.collapse}>
-          <Card>
-            <CardBody>
-            Anim pariatur cliche reprehenderit,
-             enim eiusmod high life accusamus terry richardson ad squid. Nihil
-             anim keffiyeh helvetica, craft beer labore wes anderson cred
-             nesciunt sapiente ea proident.
-            </CardBody>
-          </Card>
+        <p>Anyone who wants to track their mood or mental health over time. Our hope is you will begin to recognize patterns and make more informed decisions based on the details of your day.</p>         
         </Collapse>
+<br />
+
+
+        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> How does this work?</span> </Button>
+        <Collapse isOpen={this.state.collapse}>
+        <p>Every day you can log in and take a short survey that will measure your feelings. You can also make journal entries. The survey responses will be graphed over time. In this way, you can look at a specific day on the graph to see the corresponding journal entries.</p>      
+        </Collapse>
+<br />
+
+
+        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Does this replace seeing a mental health professional?</span> </Button>
+        <Collapse isOpen={this.state.collapse}>
+        <p>No. This is a tool you may choose to use with your therapist, but it is not intended to supplement professional mental health care.</p>
+  
+        </Collapse>
+<br />
+
+
+        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Why don't I see my specific diagnosis on here?</span> </Button>
+        <Collapse isOpen={this.state.collapse}>
+        <p>We chose to focus on anxiety and depression because they are the most commonly diagnosed mood disorders.</p>     
+  
+        </Collapse>
+<br />
+
+
       </div>
     );
   }

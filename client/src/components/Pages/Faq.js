e62370183669772555cb7ee387122424cb0e7790
alyciamriley <alyciarileym@gmail.com>
2018-05-02 20:37:18 -0500

    made some color changes.

--- client/src/components/Pages/Faq.js
+++ client/src/components/Pages/Faq.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import { Collapse, Button, } from 'reactstrap';
 import "./PagesStyles/Faq.css";
 
 class Faq extends Component {
@@ -16,31 +16,31 @@ class Faq extends Component {
   render() {
     return (
       <div>
-        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Who is this for?</span> </Button>
+        <Button color="link" className="btn" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Who is this for?</span> </Button>
         <Collapse isOpen={this.state.collapse}>
-        <p>Anyone who wants to track their mood or mental health over time. Our hope is you will begin to recognize patterns and make more informed decisions based on the details of your day.</p>         
+        <p className="answer">Anyone who wants to track their mood or mental health over time. Our hope is you will begin to recognize patterns and make more informed decisions based on the details of your day.</p>         
         </Collapse>
 <br />
 
 
-        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> How does this work?</span> </Button>
+        <Button color="link" className="btn" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> How does this work?</span> </Button>
         <Collapse isOpen={this.state.collapse}>
-        <p>Every day you can log in and take a short survey that will measure your feelings. You can also make journal entries. The survey responses will be graphed over time. In this way, you can look at a specific day on the graph to see the corresponding journal entries.</p>      
+        <p className="answer">Every day you can log in and take a short survey that will measure your feelings. You can also make journal entries. The survey responses will be graphed over time. In this way, you can look at a specific day on the graph to see the corresponding journal entries.</p>      
         </Collapse>
 <br />
 
 
-        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Does this replace seeing a mental health professional?</span> </Button>
+        <Button color="link" className="btn"onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Does this replace seeing a mental health professional?</span> </Button>
         <Collapse isOpen={this.state.collapse}>
-        <p>No. This is a tool you may choose to use with your therapist, but it is not intended to supplement professional mental health care.</p>
+        <p className="answer">No. This is a tool you may choose to use with your therapist, but it is not intended to supplement professional mental health care.</p>
   
         </Collapse>
 <br />
 
 
-        <Button color="link" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Why don't I see my specific diagnosis on here?</span> </Button>
+        <Button color="link" className="btn" onClick={this.toggle} style={{ marginBottom: '1rem' }}><i class="fas fa-angle-double-right"></i> <span className ="header"> Why don't I see my specific diagnosis on here?</span> </Button>
         <Collapse isOpen={this.state.collapse}>
-        <p>We chose to focus on anxiety and depression because they are the most commonly diagnosed mood disorders.</p>     
+        <p className="answer">We chose to focus on anxiety and depression because they are the most commonly diagnosed mood disorders.</p>     
   
         </Collapse>
 <br />
--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-USE moodmapper;
-INSERT INTO moods (anxiety, depression, concentration, energy, happiness, createdAt, updatedAt)
-VALUES (2, 4, 5, 3, 2, current_timestamp(), current_timestamp());
+-- USE moodmapper;
+-- INSERT INTO moods (anxiety, depression, concentration, energy, happiness, createdAt, updatedAt)
+-- VALUES (2, 4, 5, 3, 2, current_timestamp(), current_timestamp());

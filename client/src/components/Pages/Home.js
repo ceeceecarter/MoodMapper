beaffc6dbae5378e1d1d1f3be038a20a28f0604a
Elizabeth <erischjanson@gmail.com>
2018-05-05 10:14:33 -0500

    content homepage and about me

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -4,11 +4,7 @@ const Home = () => (
   <div>
     <h1>Home page</h1>
     <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
+     Welcome to Mood Mapper, an app that help you track your moods from day to day.
     </p>
   </div>
 );

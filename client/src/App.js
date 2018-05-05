9fd073383466f3d929bbfe21b0a1d77429dcc0c4
alyciamriley <alyciarileym@gmail.com>
2018-05-05 17:20:12 -0500

    code cleanup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,7 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Container, Row, Col } from "reactstrap";
+import { BrowserRouter as Router, Switch, Link } from "react-router-dom";
+
 import Wrapper from "./components/Wrapper";
-import NavTabs from "./components/NavTabs";
-import SidebarContainer from "./components/Sidebar";
-import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
-import Faq from "./components/Pages/Faq";
-import Graph from "./components/Pages/Graph";
-import History from "./components/Pages/History";
-import Home from "./components/Pages/Home";
-import Profile from "./components/Pages/Profile";
-import Survey from "./components/Pages/Survey";
 import Landing from "./components/Pages/Landing"
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 
 import ReactDOM from "react-dom";
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
-// import Landing from "../src/components/Pages/Landing";
 import registerServiceWorker from "./registerServiceWorker";
 
-<Route exact path="/App" component={App} />
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();

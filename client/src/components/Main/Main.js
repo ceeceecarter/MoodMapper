43db6d340758ae4f23db30813792919403b97438
alyciamriley <alyciarileym@gmail.com>
2018-05-05 11:49:55 -0500

    working on landing page with nested components.

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import Wrapper from "./components/Wrapper";
+import NavTabs from "./components/NavTabs";
+import SidebarContainer from "./components/Sidebar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import Faq from "./components/Pages/Faq";
+import Graph from "./components/Pages/Graph";
+import History from "./components/Pages/History";
+import Home from "./components/Pages/Home";
+import Profile from "./components/Pages/Profile";
+import Survey from "./components/Pages/Survey";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+          <Container className="reactstrapContainer">
+            <Row>
+              <Col lg="12" className="NavTabs">
+                <NavTabs />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="2" className="sidebar">
+                <SidebarContainer />{" "}
+              </Col>
+              <Col lg="10" className="home">
+                <Switch>
+                  {/* <Route exact path="/" component={Landing} /> */}
+                  <Route exact path="/Survey" component={Survey} />
+                  <Route exact path="/Graph" component={Graph} />
+                  <Route exact path="/Faq" component={Faq} />
+                  <Route exact path="/Profile" component={Profile} />
+                  {/* <Route component={NoMatch} /> */}
+                </Switch>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="12" className="footer">
+                <Footer />
+              </Col>
+            </Row>
+          </Container>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- client/src/components/Pages/Landing.js
+++ client/src/components/Pages/Landing.js
@@ -1,34 +1,32 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch , Link} from "react-router-dom";
-
-import { Container, Row, Col } from "reactstrap";
+import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import Wrapper from "../Wrapper";
 import Login from "../Login";
 import App from "../../App";
+import "../../UserRoutes/html-routes.js"
 import "./PagesStyles/Landing.css";
 
-class Landing extends Component {   
-    render() {
-      return (
-        <Router>
-          <Wrapper>
-      
-              <Row>
-                <Col lg="8" className="blank-area">
-                  
-                </Col>
-                <Col lg="4" className="login-container">
-                <Login />
 
-                {/* <Link to ="../../App"> main app is here</Link> */}
-                 
-                </Col>
-              </Row>
-            
-          </Wrapper>
-        </Router>
-      );
-    }
+class Landing extends Component {   
+  render() {
+    return (
+      <Router>
+        <Wrapper> 
+    
+            <Row>
+              <Col lg="8" className="blank-area">
+                
+              </Col>
+              <Col lg="4" className="login-container">
+              <Login /> 
+              </Col>
+            </Row>
+            <Link to ='/Main' >Main</Link>
+        </Wrapper>
+      </Router>
+    );
   }
-  
-  export default Landing;
+}
+
+export default Landing;
